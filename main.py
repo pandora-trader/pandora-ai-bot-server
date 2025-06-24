05772730077fe9377b9703000f3bb5565b726e9e
pandora-trader <abduraximaxunov701@gmail.com>
2025-06-24 15:41:31 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, Request
 
-app = FastAPI()
+ilova = FastAPI()
 
-@app.get("/")
+@ilova.get("/")
 async def ildiz():
     return {"message": "Pandora AI bot server ishlayapti."}
 
-@app.post("/webhook")
+@ilova.post("/webhook")
 async def webhook(iltimos: Request):
     ma_lumotlar = await iltimos.json()
     print("Telegram'dan kelgan webhook:")
