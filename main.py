1865a4493f18c04687da28d92affcdad2408d357
pandora-trader <abduraximaxunov701@gmail.com>
2025-06-24 15:43:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from fastapi import FastAPI, Request
+import requests
 
-ilova = FastAPI()
-
-@ilova.get("/")
-async def ildiz():
-    return {"message": "Pandora AI bot server ishlayapti."}
+TOKEN = "8022008632:AAF0NyLr-ALlaJaZ-oNOI8qSAoROuoJc_Qw"
+API_URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
 @ilova.post("/webhook")
 async def webhook(iltimos: Request):
     ma_lumotlar = await iltimos.json()
     print("Telegram'dan kelgan webhook:")
     print(ma_lumotlar)
+
+    # Telegram'dан хабар юборган фойдаланувчини ID си
+    user_id = ma_lumotlar["message"]["from"]["id"]
+    msg = ma_lumotlar["message"]["text"]
+
+    # Жавоб тайёрлаш
+    javob = f"Салом! Сиз ёздингиз: {msg}"
+
+    # Жавобни Telegram'га юбориш
+    requests.post(API_URL, json={
+        "chat_id": user_id,
+        "text": javob
+    })
+
     return {"kelib tushdi": True}
